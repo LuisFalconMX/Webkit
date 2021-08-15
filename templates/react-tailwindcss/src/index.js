b93c90ada3729ef57d84720b6a93fd0ff1a57419
luisfalconmx <luisfalconmx@gmail.com>
2021-08-14 21:22:56 -0500

    feat: the layout was implemented with the Navbar and Footer components

--- templates/react-tailwindcss/src/index.js
+++ templates/react-tailwindcss/src/index.js
@@ -2,18 +2,22 @@ import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import 'tailwindcss/tailwind.css'
+import Navbar from '@components/Navbar'
+import Footer from '@components/Footer'
 
 const Home = lazy(() => import('@routes/Home'))
 const NotFound = lazy(() => import('@routes/NotFound'))
 
 ReactDOM.render(
   <BrowserRouter>
+    <Navbar />
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route component={NotFound} />
       </Switch>
     </Suspense>
+    <Footer />
   </BrowserRouter>,
   document.getElementById('app')
 )
