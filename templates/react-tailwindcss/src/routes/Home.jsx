0ad8012d599bf8be889d81c632e1651586a808ba
luisfalconmx <luisfalconmx@gmail.com>
2021-08-15 16:17:51 -0500

    feat: support for custom hooks

--- templates/react-tailwindcss/src/routes/Home.jsx
+++ templates/react-tailwindcss/src/routes/Home.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { useTranslation } from 'react-i18next'
+import useCounter from '@hooks/useCounter'
 
 const Home = () => {
   const { t, i18n } = useTranslation()
-
-  const changeLanguage = (lang) => {
-    i18n.changeLanguage(lang)
-  }
+  const { counter, increase, decrement, reset } = useCounter()
 
   return (
     <>
       <Helmet>
         <title>This is title for home page</title>
       </Helmet>
+
       <h1>{t('title')}</h1>
-      <button onClick={() => changeLanguage('en')}>English</button>
-      <button onClick={() => changeLanguage('es')}>Spanish</button>
+      <button onClick={() => i18n.changeLanguage('en')}>English</button>
+      <button onClick={() => i18n.changeLanguage('es')}>Spanish</button>
+
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <div>{counter}</div>
+      <button onClick={decrement}>-</button>
+      <br />
+      <button onClick={reset}>reset</button>
+      <br />
+      <button onClick={increase}>+</button>
+      <br />
     </>
   )
 }
